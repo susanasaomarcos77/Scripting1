d25c8cc4401905675385739d74db1f506eeae0bb
susanasaomarcos77 <160172152+susanasaomarcos77@users.noreply.github.com>
2024-02-21 17:17:13 +0000

    Tpc curso

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {//The object you will destroy is named "bola"
+
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
